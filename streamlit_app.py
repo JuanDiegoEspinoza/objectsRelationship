590b7f51f0dd91d90dd479a1dc1bba41f06f679f
juandiegoes <JuanDiegoEspinoza@users.noreply.github.com>
2024-06-26 15:27:56 -0600

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,25 +17,25 @@ if st.button('Read'):
     while i < len(x):
         parentName = x.iloc[i]['ob1']
         childName = x.iloc[i]['ob2']
-        parentFlag = 0
-        childFlag = 0
         
-        newParentNode = Node(parentName)
-        listNodesNames +=[parentName]
-        listNodesObjets+= [newParentNode]
+        if(parentName in listNodesNames):
+            newParentNode = listNodesObjets[listNodesNames.index(parentName)]
+            parentFlag = 1
+        else: 
+            newParentNode = Node(parentName)
+            listNodesNames +=[parentName]
+            listNodesObjets+= [newParentNode]
 
-        #if(parentName in listNodesNames):
-        #    newParentNode = listNodesObjets[listNodesNames.index(parentName)]
-        #    parentFlag = 1
 
-        #if(childName in listNodesNames):
-        #    newChildNode = listNodesObjets[listNodesNames.index(childName)]
-        #    childFlag = 1         
-        
-        newChildNode = Node(childName, parent=newParentNode)
-        listNodesNames +=[childName]
-        listNodesObjets+= [newChildNode]
+        if(childName in listNodesNames):
+            newChildNode = listNodesObjets[listNodesNames.index(childName)]
+            childFlag = 1
+        else:       
+            newChildNode = Node(childName, parent=newParentNode)
+            listNodesNames +=[childName]
+            listNodesObjets+= [newChildNode]
         i+=1
+
     for i in listNodesObjets:
         root = i
         render.RenderTree(root).by_attr()
