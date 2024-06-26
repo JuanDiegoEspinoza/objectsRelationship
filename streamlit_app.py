81bc4f42f3bf5fefc6d9deac1ad8dd4ce9862184
juandiegoes <JuanDiegoEspinoza@users.noreply.github.com>
2024-06-26 14:34:35 -0600

    trest

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,10 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-test = st.file_uploader('the notice you want to tell')
 
-pd.read_excel(test)
+uploadedFile = st.file_uploader()
+
+x = pd.read_excel(uploadedFile)
 
 if st.button('Read'):
     st.write(x)
