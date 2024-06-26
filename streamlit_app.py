6649b392e1e8b5528779c268dfc8d5639bf01fea
juandiegoes <JuanDiegoEspinoza@users.noreply.github.com>
2024-06-26 15:09:46 -0600

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,39 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
+from anytree import Node, RenderTree
+from anytree.exporter import UniqueDotExporter
 
 
 uploadedFile = st.file_uploader("Choose")
 
 x = pd.read_csv(uploadedFile)
+listNodesNames = []
+listNodesObjets = []
 
 if st.button('Read'):
     i = 0
     while i < len(x):
-        st.write(x.iloc[i])
-        st.write(x.iloc[i]['ob1'])
+        parentName = x.iloc[i]['ob1']
+        childName = x.iloc[i]['ob2']
+        parentFlag = 0
+        childFlag = 0
+
+        if(parentName in listNodesNames):
+            newParentNode = listNodesObjets[listNodesNames.index(parentName)]
+            parentFlag = 1
+
+        if(childName in listNodesNames):
+            newChildNode = listNodesObjets[listNodesNames.index(childName)]
+            childFlag = 1
+        
+        if(parentFlag=0):
+            newParentNode = Node(parentName)
+            listNodesNames +=[parentName]
+            listNodesObjets+= [newParentNode]
+        
+        if (childFlag=0):
+            newChildNode = Node(newParentNode, childName)
+            listNodesNames +=[childName]
+            listNodesObjets+= [newChildNode]
         i+=1
